956652292b9bee04276c8dbb10579cc4fb8f1d1b
Melike <melikedemirci23@gmail.com>
2024-04-20 11:13:28 +0300

    Update App.jsx
    JSON file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [monsters, setMonsters] = useState([
-    { name: 'Linda' },
-    { name: 'Frank' },
-    { name: 'Jacky' },
-    { name: 'Andrei' }
-  ]);
+  const [monsters, setMonsters] = useState([]);
 
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(Response => Response.json())
+      .then(users => {
+        //API'den gelen veriyi monsters state'ine ayarla
+        setMonsters(users);
+        // Konsola veriyi yazdır
+        console.log('API verisi:', users);
+      })
+      .catch(error => {
+        //Hata durumunda işlem yap
+        console.error('Veri alınamadı: ', error);
+      });
+  }, []); //Bu effect sadece bir kez çalışacak, bileşen yüklendiğinde
 
   return (
     <div className="App">
-      {monsters.map((monster,index)=>{
+      {monsters.map((monster, index) => {
         return <h1 key={index}>{monster.name}</h1>
       })}
     </div>
-)
+  )
 }
 
 export default App
